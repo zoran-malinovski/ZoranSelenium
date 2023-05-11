f855096982a2a6084d6b37b7f14d3a33237843ba
Zoran <z.malinovski79@gmail.com>
2023-05-11 20:07:00 +0200

    made first test for SeleniumTest

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,2 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SeleniumTest {
+
+
+    public static void main(String[] args) {
+
+        //Create a new instance of the Chrome driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Chrome Drivers\\chromedriver.exe");
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        //navigation on page
+        driver.get("https://www.google.com/");
+
+        //Create Object of Element class in Selenium and find text input element by name="q" in search bar;
+        WebElement element = driver.findElement(By.name("q"));
+
+        driver.manage().window().maximize();
+        //Enter text in search bar
+        element.sendKeys("motogp");
+        element.submit();
+
+        //click on link
+        WebElement element1 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div[1]/a"));
+        element1.click();
+
+        //Click on Enable button
+        WebElement element2 = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        element2.click();
+
+
+        //check page title is moto gp - Prebaruvanje na google
+        System.out.println("Page title is : " +driver.getTitle());
+
+        //close the Browser : "quite" to close browser; "close" to close one tab
+        //driver.quit();
+
+
+
+
+    }
 }
