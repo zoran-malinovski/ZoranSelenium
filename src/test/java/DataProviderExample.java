fe800dfbfd39a9e255c599a56f63baab01571f9e
Zoran <z.malinovski79@gmail.com>
2023-05-11 18:40:10 +0200

    added DataProvider Object and Test DataProvider

--- src/test/java/DataProviderExample.java
+++ src/test/java/DataProviderExample.java
@@ -1,2 +1,20 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class DataProviderExample {
+    //primitivni i referentni tipovi (niza-referntni tipovi na podatoci;
+    // i matrica-primitivni tipovi na podatoci)
+    // [][] - matrica
+    // [] - niza
+
+    @Test(dataProvider = "LoginDataProvider")
+    public void loginTest(String email, String pass){
+        System.out.println("name and password: " + email +"/"+ pass);
+    }
+
+    @DataProvider(name = "LoginDataProvider")
+    public Object[][] getData(){
+        Object[][] data = {{"zoki@gmail.com","zoki"}, {"zoran@gmail.com","zoran"}, {"mali@gmail.com","mali"}};
+                return data;
+    }
 }
